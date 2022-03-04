c08266f6039539ac86547fec75b0e84e2690b395
이 성 노 <kulbo0926@gmail.com>
2022-03-04 15:03:58 +0900

    코트리단에서 바인딩 변수 설정

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -2,10 +2,14 @@ package com.neppplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(R.layout.activity_sign_up)
     }
 }
