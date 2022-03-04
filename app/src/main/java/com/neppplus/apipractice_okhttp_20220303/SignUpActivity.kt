8616d10d81572e366cc3e8c370dae9425ee90285
이 성 노 <kulbo0926@gmail.com>
2022-03-04 15:13:01 +0900

    BaseActivity를 물려반아, 의무 구형 함수 2개작성 /onCreate에서 실제 실행

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(R.layout.activity_sign_up)
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+
 }
