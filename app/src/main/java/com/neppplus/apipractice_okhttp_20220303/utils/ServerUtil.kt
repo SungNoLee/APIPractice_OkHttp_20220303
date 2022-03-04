2ebb0105ac845ea2fd2677a62111c77ff446ae14
이 성 노 <kulbo0926@gmail.com>
2022-03-04 10:44:54 +0900

    서버 응답의 본문을 String으로 변환, 로그

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -1,7 +1,8 @@
 package com.neppplus.apipractice_okhttp_20220303.utils
 
-import okhttp3.FormBody
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
 
 class ServerUtil {
 
@@ -27,8 +28,32 @@ class ServerUtil {
                 .url(urlString)
                 .post(formData)
                 .build()
+//            종합한 Request도 실제 호출을 해 줘야 API 호출이 실행됨.(startActivity 같은 동자 필요함)
+//            실제 호출 : 클라이언트로써 동작 > OkHttpClient 클래스
+            val client = OkHttpClient()
 
+//           OkHttpClient 객체를 이용 > 서버에 로그인 기능 실제 호출
+//            호출을 했으면, 서버가 수행한 결과를 받아서 처리.
+//            => 서버에 다녀와서 할 일을 등록 : enqueue(Callback)
 
+            client.newCall(request).enqueue( object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+//                실패 : 서버 연결 자체를 실패. 응답이 오지 않았다.
+//                    ex. 인터넷 끊김, 서버 접속 불가 등등 물리적 연결 실패
+//                    ex. 비번 틀려서 로그인 실패 : 서버 연결 성공, 응답도 돌아왔는데 -> 그 내용만 실패.(물리적 실패 x)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    어떤 내용이던, 응답 자체는 잘 돌아온 경우. (그 내용은 성공/실패 일 수 있다)
+//                    응답 : reponse  변수 > 응답의 본문 (body) 만 보자.
+
+                    val bodyString = response.body!!.string()   // toString() 아님! string() 기능은 1회용.
+                    Log.d("서버테스트", bodyString)
+
+                }
+
+            })
         }
     }
 }
