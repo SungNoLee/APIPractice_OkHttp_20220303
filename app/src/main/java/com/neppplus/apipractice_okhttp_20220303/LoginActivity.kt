61a38b82c14c5db40f7c02ea7a11d30ad2db4221
이 성 노 <kulbo0926@gmail.com>
2022-03-07 09:42:28 +0900

    메인액티비티 이름 변경 > 로그인액티비티

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/LoginActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/LoginActivity.kt
@@ -1,20 +1,19 @@
 package com.neppplus.apipractice_okhttp_20220303
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity :  BaseActivity() {
-    lateinit var binding : ActivityMainBinding
+class LoginActivity :  BaseActivity() {
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         setupEvents()
         setValues()
 
--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -1,10 +1,8 @@
 package com.neppplus.apipractice_okhttp_20220303
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivitySignUpBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
