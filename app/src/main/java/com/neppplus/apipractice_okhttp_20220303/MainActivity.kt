f1cff2fd37564982601dc96c0a4ff50af627fb41
이 성 노 <kulbo0926@gmail.com>
2022-03-07 17:10:01 +0900

    TopicData 객체를 만들어서 내용물들을 topicObj의 JSON내용물들로 대임.  - 완료 후 목록에 추가

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.neppplus.apipractice_okhttp_20220303.datas.TopicData
@@ -36,7 +37,24 @@ class MainActivity : BaseActivity() {
                 val topicsArr = dataObj.getJSONArray("topics")
 
 //                topicsArr 내부를 하나씩 추출(JSONObject{ }) => TopicData() 로 변환.
+//                JSONArray는 for-each 문법 지원 X. (차후 : ArrayList의 for-each 활용 예정)
+//                JAVA : for (int i=0; i < 배열.length; i++) 와 완전히 동일한 문법.
+                for (i in 0 until topicsArr.length()) {
+//                    [] => {}, {}, 순서에 맞는 {}를 변수에 담자.
+//                    JSON파싱의 {} => (JSONArray에게서) JSONObject로 추출
 
+                    val topicObj = topicsArr.getJSONObject( i )
+                    Log.d("받아낸주제", topicObj.toString())
+//                    TopicData 변수 생성 => 멤버변수에 topicObj가 들고있는 값들을 대입.
+                    val topicData = TopicData()
+                    topicData.id = topicObj.getInt("id")
+                    topicData.title = topicObj.getString("title")
+                    topicData.imageURL = topicObj.getString("img_url")
+                    topicData.replyConunt = topicObj.getInt("reply_count")
+
+//                    완성된 TopicData 객체를 목록에 추가.
+                    mTopicList.add(topicData)
+                }
             }
 
         })
