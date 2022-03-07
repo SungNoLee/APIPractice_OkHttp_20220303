6a1fbfa1421d339761be09b6703e7cd0a267cf60
이 성 노 <kulbo0926@gmail.com>
2022-03-07 10:54:36 +0900

    EditText의 이벤트 처리 체험

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.apipractice_okhttp_20220303
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivitySignUpBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
@@ -18,12 +20,32 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        binding.edtEmail.addTextChangedListener {
+            Log.d("입력내용", it.toString())
+        }
         binding.btnEmailCheck.setOnClickListener {
 //            입력 이메일 값 추출
             val inputEmail = binding.edtEmail.text.toString()
 //            서버의 중복확인 기능(user_check - GET) API 활용 > ServerUtil에 함수 추가, 가져다 활용.
 //            그 응답 code값에 따라 다은 문구 배치.
-            ServerUtil.getRequestDuplicatedCheck("EMAIL", inputEmail, null)
+//            obect 는 익명 클래스
+            ServerUtil.getRequestDuplicatedCheck("EMAIL", inputEmail, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+                        when(code) {
+                            200 -> {
+                                binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
+                            }
+                            else -> {
+                                binding.txtEmailCheckResult.text = "다른 이메일로 다시 시도해 주세요."
+                            }
+                        }
+                    }
+                }
+
+            })
         }
         binding.btnSignUp.setOnClickListener {
             val inputEmail = binding.edtEmail.text.toString()
