a59846fd558d62deae7e422b346011d7c08510ba
이 성 노 <kulbo0926@gmail.com>
2022-03-07 16:55:46 +0900

    topics라는 Array 추출

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.neppplus.apipractice_okhttp_20220303.datas.TopicData
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
     lateinit var binding : ActivityMainBinding
+//    실제로 서버
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,18 +22,24 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
-            override fun onResponse(jsonObject: JSONObject) {
-                val dataObj = jsonObject.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
-                runOnUiThread {
-                    binding.txtLoginUserName.text = nickname
-                }
+
+//        메인 화면 정보를 가져오기 => API 호출 / 응답 처리
+        getTopicListFromServer()
+    }
+
+    fun getTopicListFromServer() {
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
+//                서버가 주는 토론 주제 목록 파싱 => mTopicList에 추가하기.
+                val dataObj = jsonObj.getJSONObject("data")
+
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//                topicsArr 내부를 하나씩 추출(JSONObject{ }) => TopicData() 로 변환.
+
             }
 
         })
-
     }
 
 }
