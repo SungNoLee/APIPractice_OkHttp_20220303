b6cdc7d3d5bc863cf44a8a79e72ea94d935ede67
이 성 노 <kulbo0926@gmail.com>
2022-03-07 12:06:40 +0900

    메모장 파일 이름 변수 생성

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ContextUtil.kt
@@ -3,6 +3,7 @@ package com.neppplus.apipractice_okhttp_20220303.utils
 class ContextUtil {
 
     companion object {
-
+//        메모장 파일 이름처럼, SharedPreferences이 이름 설정.
+        private val prefName = "OkHttpPracticePref"
     }
 }
