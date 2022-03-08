a29ab242fd8e6dc5a5f1054151388e5ef2b57a5c
이 성 노 <kulbo0926@gmail.com>
2022-03-08 16:52:25 +0900

    UserData 클래스 추가 + ReplyData 파싱 활용  - 리스크뷰에 사용할 데이터클래스 준비

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -7,6 +7,8 @@ class ReplyData(
     var content: String
 )  {
 
+    var writer : UserData() // 모든 뎃글에는 작성자가 있다. null 가능성이 없다.
+
     //    보조 생성자 추가 연습 : 파라미터 x.
     constructor() : this(0, "내용없음")
 
@@ -15,6 +17,9 @@ class ReplyData(
             val replyData = ReplyData()
 
 //            JSON 정보 > 멤버변수 채우기
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+            replyData.writer = UserData.getUserDataFromJson(jsonObj.getJSONObject("user"))
             return replyData
         }
     }
