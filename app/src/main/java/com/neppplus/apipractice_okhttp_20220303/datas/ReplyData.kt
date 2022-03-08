e09054e6b09c61dee226dff5afdee9abe76e82be
이 성 노 <kulbo0926@gmail.com>
2022-03-08 16:59:50 +0900

    만들어져있는 Sidedata를 활용해서 댓긍의 지지 진영도 파싱

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -9,6 +9,8 @@ class ReplyData(
 
     var writer : UserData() // 모든 뎃글에는 작성자가 있다. null 가능성이 없다.
 
+    var selectedSide = SideData()
+
     //    보조 생성자 추가 연습 : 파라미터 x.
     constructor() : this(0, "내용없음")
 
@@ -20,6 +22,8 @@ class ReplyData(
             replyData.id = jsonObj.getInt("id")
             replyData.content = jsonObj.getString("content")
             replyData.writer = UserData.getUserDataFromJson(jsonObj.getJSONObject("user"))
+
+            replyData.selectedSide = SideData.getSideDataFromJson(jsonObj.getJSONObject("selected_side"))
             return replyData
         }
     }
